29373377a51d7fd8e7d764f1428106a1425fc4c5
Brandon Walden <brandonshanewalden@gmail.com>
2017-07-26 19:11:37 -0400

    still need completed list and button fix

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const mustacheExpress = require('mustache-express');
 const bodyParser = require('body-parser');
 const expressValidator = require('express-validator')
+const toDos = [];
+const complete = [];
 
 var app = express();
 
@@ -10,43 +12,26 @@ app.set('views', './views');
 app.set('view engine', 'mustache');
 
 app.use(express.static('public'));
-
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
   extended: false
 }));
 
-app.get('/', function(request, response) {
-  response.render('thingsToDo');
+app.get("/", function(req, res) {
+  res.render('thingsToDo', {
+    toDos: toDos, complete: complete
+  });
 });
-// 
-// app.post('/', function(request, response) {
-//   var schema = {
-//     'name': {
-//       notEmpty: true,
-//       isLength: {
-//         options: [{
-//           max: 100
-//         }],
-//         errorMessage: 'Name must not be longer than 100 characters'
-//       },
-//       errorMessage: 'Invalid Name'
-//     }
-//   request.assert(schema);
-//   request.getValidationResult().then(function(results) {
-//     if (results.isEmpty()) {
-//       response.render('thingsToDo', {
-//         answers: request.body
-//       });
-//     } else {
-//       response.render('thingsToDo', {
-//         errors: results.array()
-//       });
-//     }
-//   });
-// });
 
+app.post("/", function(req, res) {
+  toDos.push(req.body.toDo);
+  res.redirect('/');
+})
 
+app.get("/", function(req, res) {
+  complete.push(toDos);
+  res.redirect('/');
+})
 
 app.listen(3000, function() {
   console.log('To Do list on...');
