1c5fadfb393d86e85ee00ff5d3683518e8727f45
Brandon Walden <brandonshanewalden@gmail.com>
2017-07-27 10:31:44 -0400

    strike out

--- index.js
+++ index.js
@@ -2,36 +2,44 @@ const express = require('express');
 const mustacheExpress = require('mustache-express');
 const bodyParser = require('body-parser');
 const expressValidator = require('express-validator')
-const toDos = [];
-const complete = [];
+let listToDos = [];
+// let complete = [];
 
-var app = express();
+const app = express();
 
 app.engine('mustache', mustacheExpress());
 app.set('views', './views');
 app.set('view engine', 'mustache');
 
 app.use(express.static('public'));
-app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
   extended: false
 }));
 
 app.get("/", function(req, res) {
   res.render('thingsToDo', {
-    toDos: toDos, complete: complete
+    toDos: listToDos,
   });
 });
 
 app.post("/", function(req, res) {
-  toDos.push(req.body.toDo);
+  let id = parseInt(Math.random() * 1000)
+  newTodoObject = {
+    text: req.body.newTodo,
+    id: id
+  }
+  listToDos.push(newTodoObject);
   res.redirect('/');
-})
+});
 
-app.get("/", function(req, res) {
-  complete.push(toDos);
+app.post('/mark-complete/:id', function(req, res) {
+  let toDoId = parseInt(req.params.id);
+  let completeTodo = listToDos.find(function (todo) {
+    return todo.id === toDoId
+  });
+  completeTodo.complete = true;
   res.redirect('/');
-})
+});
 
 app.listen(3000, function() {
   console.log('To Do list on...');
